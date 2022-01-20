f94eae34667921c12610525781f81fa6077211e0
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2022-01-20 20:55:28 +0200

    Initial commit

---
+++ app/src/main/java/com/chudnovskiy/lab_1_2_colorused/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chudnovskiy.lab_1_2_colorused
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
